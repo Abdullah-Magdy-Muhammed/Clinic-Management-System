26a206a7252bb06d210ca823a4044e921d1367e6
Ahhmedshams <ahmedtaha757@gmail.com>
2023-03-18 16:20:05 +0200

    appt

--- routes/appointment.js
+++ routes/appointment.js
@@ -12,7 +12,7 @@ const router = express.Router({caseSensitive:false});
 
 
 router.route("/")
-.get(advancedResults(appointment,[{ path:"doctorId" , select: { _id:0 , name:1 } },{ path:"patientId" , select: { _id:0 , name:1 } },{ path:"clinicId" , select: { _id:0 ,name:1, location:1 } }]),controller.getAppointment)
+.get(advancedResults(appointment,[{ path:"doctorId" , select: { _id:0 , name:1, clinicId:1 } },{ path:"patientId" , select: { _id:0 , name:1 } } ]),controller.getAppointment)
 .post(validation.appointmentPost,validator,controller.createAppointment)
 
 
