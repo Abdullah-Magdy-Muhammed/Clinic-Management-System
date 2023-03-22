cdcfb95626b01c5d72cc86465b20e770943f9982
Abdullah Magdy <magdyabdullah200@gmail.com>
2023-03-22 19:23:25 +0200

    bcrypt password when update user

--- routes/employee.js
+++ routes/employee.js
@@ -1,31 +1,31 @@
 const express = require("express");
-const validator =require("../middlewares/errorValidation")
+const validator = require("../middlewares/errorValidation")
 const validation = require("./../middlewares/validations");
-const employeeController=require("../controller/employee");
-const advancedResults = require ("./../middlewares/advancedResult");
+const employeeController = require("../controller/employee");
+const advancedResults = require("./../middlewares/advancedResult");
 require('./../model/employee');
 const mongoose = require("mongoose")
 //const allowedUsers =require("./../middlewares/AuthorizeRole");
 
-const employee= mongoose.model('employee');
+const employee = mongoose.model('employee');
 
 const router = express.Router();
 //Without Id
 router.route("/employee")
-.get(advancedResults(employee,{ path:"clinicId" , select: { _id:0 , name:1 } }),employeeController.getAllEmployees)
-.post(validation.employeePost,validator,employeeController.addEmployee)
+    .get(advancedResults(employee, { path: "clinicId", select: { _id: 0, name: 1 } }), employeeController.getAllEmployees)
+    .post(validation.employeePost, validator, employeeController.addEmployee)
 
 
 //Route ID
 router.route("/employee/:id")
-.get(
-    validation.paramIdInt,validator,employeeController.getEmployeeById)
-.delete(
-    validation.paramIdInt,validator,employeeController.deleteById)
-.patch(
-    validation.paramIdInt,validator,employeeController.updateEmployee)
+    .get(
+        validation.paramIdInt, validator, employeeController.getEmployeeById)
+    .delete(
+        validation.paramIdInt, validator, employeeController.deleteById)
+    .patch(
+        validation.paramIdInt, validator, employeeController.updateEmployee)
 
 router.route("/employee/:id/status")
-.patch(validation.updateStatus,validator,employeeController.updateEmployeeStaus)
-     
-module.exports=router;
+    .patch(validation.updateStatus, validator, employeeController.updateEmployeeStaus)
+
+module.exports = router;
